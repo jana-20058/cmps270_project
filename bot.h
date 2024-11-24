0b3372e94258a551d3a16e0d2108c6529ad1f7d2
jana <janajafal86@gmail.com>
2024-11-24 22:55:30 +0200

    added variables and shipSunkByBot function, removed shuffle function, and updated some prototypes

--- bot.h
+++ bot.h
@@ -1,15 +1,20 @@
 #ifndef BOT_H
 #define BOT_H
+int misses = 0;
+ int fires = 0;
+int totalNumberOfShipsSunkByBot = 0;
+int flagShipSunkInCurrentTurn = 0;
+
 char matchingCharacters(int index);
-void shuffle(int *array);
 void ShipPlacment(char **grid);
 void initializeHeatGrid(int** heatGrid);
-void generateHeatmap(int* shipSizes,  int** heatmap);
+void generateHeatmap(int *shipSizes, int **heatmap, char **DisplayedBot);
 void updateHeatMap(int col,int row,char result,char *move,int ** heatGrid);
 int *heatmapvalue(int **heatmap, char **DisplayGridBot);
 void FireBot(char** opponentGrid, int** heatmap, char** DisplayGridBot,int* ship);
-void botmove(char**oponentGrid,int **heatmap,int flag,int CountershipsSunk, int smokeScreensUsedBot,int radarSweepsBot,char**DisplayedGridBot,int* ship);
+int botmove(char **oponentGrid, int **heatmap, int smokeScreensUsedBot, int radarSweepsBot, char **DisplayedGridBot, int *ship);
 int matchingIndecies(char cell);
 int *checkedge(char **DisplayedGridBot);
-char updateDisplayedGridBot(char **opponentGrid,char**DisplayGridBot,int nexti,int nextj,int*ship );
+char updateDisplayedGridBot(char **opponentGrid, char **DisplayGridBot, int nexti, int nextj, int *ship,int ** heatmap);
+int  ShipsSunkByBot(int *ship);
 #endif
