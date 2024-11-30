3c0b5075e48bceb6893fec5580b99cc845886645
haladaher2024 <haladaher5555@gmail.com>
2024-11-30 23:56:04 +0200

    added the headers of artillery function and its helper function

--- bot.h
+++ bot.h
@@ -12,9 +12,10 @@ int flagShipSunkInCurrentTurn = 0;
 int smokeScreensUsedBot=0;
 int radarSweepsUsedBot=0;
 coordinate targetList[MAX_TARGETS];
-bool** sunkships(int** ship,int**opponentGrid,int** DisplayedBotGrid);
+bool** sunkships(int** ship,char**opponentGrid,char** DisplayedBotGrid);
 char matchingCharacters(int index);
 void ShipPlacment(char **grid);
+char  GetGreatestArea(int** heatmap,int row,int col);
 void initializeHeatGrid(int** heatGrid);
 void generateHeatmap(int *shipSizes, int **heatmap, char **DisplayedBot);
 void updateHeatMap(int col,int row,char result,char *move,int ** heatGrid);
@@ -28,10 +29,12 @@ int  ShipsSunkByBot(int *ship);
 void torpedo(char **opponentGrid, char **DisplayedBotGrid, int *ship,int ** heatmap);
 char matching(int index);
 int SmokeScreenBot(int **smokeGrid, int shipsSunk, int smokeScreensUsedBot);
-
+void choosenGrid(char direction, int col, int row, int** heatmap, char** opponentGrid, char** DisplayedBotGrid, int* ship);
+void ArtilleryBot(int**heatmap,int *ship, char**DisplayedBotGrid, int **RadarGrid, char **opponentGrid);
 void torpedoCol(char** DisplayedBotGrid,int columnToApplyTorpedo,char** opponentGrid,int* ship);
 void torpedoRow(char** DisplayedBotGrid,int rowToApplyTorpedo,char** opponentGrid,int* ship);
 bool isAdjacentToSunkShip(int row, int col, char **opponentGrid, int *ship);
 int compareCells(const void *a, const void *b, void *heatmap_void);
+void findHighest2x2Grid(int** heatmap, int**Grid);
 int RadarSweepBot(char **opponentGrid, char **displayedGrid, int radarSweepsUsedBot, int **heatmap, int *ship);
 #endif
